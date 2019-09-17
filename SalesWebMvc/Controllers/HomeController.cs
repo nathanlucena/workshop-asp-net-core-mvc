2d2f809fe08f794ac49abca4cd8d49f3930fc2a9
Nathan <nathan.lucena89@gmail.com>
2019-09-17 20:45:38 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["email"] = "email@gmail.com";
 
             return View();
         }
