43a001c5d0c014ac3509efd56ff37a278d71b65a
Nathan <nathan.lucena89@gmail.com>
2019-09-17 21:55:23 -0300

    First Model Controller View - Depatment

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-            ViewData["email"] = "email@gmail.com";
+            ViewData["Message"] = "Salles web MVC App from c# Course.";
+            ViewData["Professor"] = "Clotiude";
 
             return View();
         }
